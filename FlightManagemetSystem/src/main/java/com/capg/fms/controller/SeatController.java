92f30f935092b13c51ec050cc7a3a37f9cf9c5b9
Nishita-art <60997293+Nishita-art@users.noreply.github.com>
2020-05-07 23:51:51 +0530

    Update SeatController.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/controller/SeatController.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/controller/SeatController.java
@@ -1,14 +1,17 @@
 package com.capg.fms.controller;
 
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.capg.fms.service.SeatService;
+import com.capg.fms.exceptions.SeatNotFoundException;
 import com.capg.fms.model.BookedSeats;
 
 
@@ -18,21 +21,60 @@ import com.capg.fms.model.BookedSeats;
 public class SeatController 
 {
 	@Autowired
-	private SeatService seatservice;
+	SeatService seatService;
 	
+	/*
+	 * This is a PostMethod(Http) is used to add the seat in the database.
+	 * Method 	 : addSeat
+	 * Type 	 : ResponseEntity<String>
+	 * parameters: 
+	 * Author 	 : Nishita Khanna
+	 * Version   :
+	 */
 	
-	@PostMapping(value="/addSeat",produces= {"application/json"})
-	public String newSeat(@RequestBody BookedSeats seat)
+	@GetMapping("/addSeat")
+	public ResponseEntity<String> addSeat(@Valid @RequestBody BookedSeats seat)
 	{
-		seatservice.newSeat(seat);
-		return "Seat added successfully";
+		seatService.newSeat(seat);
+		return new ResponseEntity <String>("Seat Added",HttpStatus.OK);
 	}
 	
+	/*
+	 * This is a GetMethod(Http) is used to get all the seat in the database.
+	 * Method 	 : getAllSeat
+	 * Type 	 : ResponseEntity<List<BookedSeats>
+	 * parameters: -
+	 * Author 	 : Nishita Khanna
+	 * Version   :
+	 */
 	
-	@GetMapping(value="/getSeat/{seatnumber}",produces={"application/json"})
-	public List<BookedSeats> displayBookedSeats(@PathVariable int seatnumber)
+	@GetMapping("/getAllSeat")
+    public ResponseEntity<List<BookedSeats>> getAllSeat( @PathVariable int seatnumber) throws SeatNotFoundException
 	{
-		return seatservice.getAllSeat(seatnumber);
+			List<BookedSeats> list = seatService.getAllSeat(seatnumber);
+			
+			if(list == null)
+				
+				throw new SeatNotFoundException("Sorry there was no data in the database");
+			
+			return new ResponseEntity<List<BookedSeats>>(list,HttpStatus.OK);
 	}
 	
+	/*
+	 * This is a GetMethod(Http) is used to get the seat in the database.
+	 * Method 	 : addSeat
+	 * Type 	 : ResponseEntity<String>
+	 * parameters: seatnumber
+	 * Author 	 : Nishita Khanna
+	 * Version   :
+	 */
+	
+	@GetMapping("/getSeat/{seatnumber}")
+	public ResponseEntity<BookedSeats> getSeat( @PathVariable int seatnumber) throws SeatNotFoundException 
+	{
+		BookedSeats list = seatService.getSeat(seatnumber);
+		if(list == null)
+			throw new SeatNotFoundException(seatnumber +""+ "Not Found in database");
+		return new ResponseEntity<BookedSeats>(list,HttpStatus.OK);
+	}
 }
