14e786c20e8287b90bbe884df2b789788eee8029
Ojaszw <60997216+Ojaszw@users.noreply.github.com>
2020-05-07 05:38:46 -0700

    Update FlightDao.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/dao/FlightDao.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/dao/FlightDao.java
@@ -6,13 +6,15 @@ import com.capg.fms.model.Flight;
 
 public interface FlightDao {
 
-	public void addFlight(Flight flight);
-	public void updateFlight(Flight flight);
+	public Flight addFlight(Flight flight);
+	public Flight updateFlight(Flight flight);
 	public Flight retrieveFlight(long flightNumber);
 	public List<Flight> retrieveAllFlights();
 	public boolean deleteFlight(long flightNumber);
 	
 }
+
+
 
 
 
