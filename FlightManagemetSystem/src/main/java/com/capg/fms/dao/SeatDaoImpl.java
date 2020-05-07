ae7d402f9e3226aeea43497bf39a6527d7642c17
Nishita-art <60997293+Nishita-art@users.noreply.github.com>
2020-05-07 23:54:02 +0530

    Update SeatDaoImpl.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/dao/SeatDaoImpl.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/dao/SeatDaoImpl.java
@@ -8,25 +8,51 @@ import javax.persistence.TypedQuery;
 import javax.persistence.EntityManager;
 
 @Repository
-public class SeatDaoImpl implements SeatDao
+public abstract class SeatDaoImpl implements SeatDao
 {
 	@PersistenceContext
 	EntityManager entityManager;
 	
+	/*
+	 * This method is used to add Seats into the BookedSeats Table.
+	 * Method 	 : addSeat
+	 * Type 	 : Boolean
+	 * parameters: seat
+	 * Author 	 : Nishita Khanna
+	 * Version 1.0
+	 */
+	
 	@Override
-	public BookedSeats newSeat(BookedSeats seat) {
+	public boolean addSeat(BookedSeats seat) {
 		entityManager.persist(seat);
-		return seat;
+		return true;
 	}
-
+	
+	/*
+	 * This method is used to add Seats into the BookedSeats Table.
+	 * Method 	 : retrieveSeat
+	 * Type 	 : BookedSeats
+	 * parameters: seatnumber
+	 * Author 	 : Nishita Khanna
+	 * Version 1.0
+	 */
+	
 	@Override
-	public BookedSeats getSeat(int seatnumber) {
+	public BookedSeats retrieveSeat(int seatnumber) {
 		return entityManager.find(BookedSeats.class, seatnumber);
 	}
-
+	/*
+	 * This method is used to add Seats into the BookedSeats Table.
+	 * Method 	 : retrieveAllSeat
+	 * Type 	 : BookedSeats
+	 * parameters: seatnumber
+	 * Author 	 : Nishita Khanna
+	 * Version 1.0
+	 */
+	
 	@Override
-	public List<BookedSeats> getAllSeat() {
-		String query = "select seats";
+	public List<BookedSeats> retrieveAllSeat(int seatnumber) {
+		String query = "select seats where booking id=" + seatnumber;
 		TypedQuery<BookedSeats> bs = entityManager.createQuery(query, BookedSeats.class);
 		return bs.getResultList();
 		
