2e202fe9963ab277c193150ea39165994770f56c
Nishita-art <60997293+Nishita-art@users.noreply.github.com>
2020-05-07 23:59:35 +0530

    Add files via upload

---
+++ FlightManagemetSystem/src/main/java/com/capg/fms/exceptions/SeatNotFoundException.java
@@ -0,0 +1,15 @@
+package com.capg.fms.exceptions;
+
+public class SeatNotFoundException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public SeatNotFoundException(String msg)
+	{
+		super(msg);
+	}
+}
