3a8f9ef781447163ab1ecef89cb09f7c11b5c137
Ojaszw <60997216+Ojaszw@users.noreply.github.com>
2020-05-07 05:43:14 -0700

    Update FlightService.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/service/FlightService.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/service/FlightService.java
@@ -1,1 +1,21 @@
 
+package com.capg.fms.service;
+
+
+import java.util.List;
+import com.capg.fms.model.Flight;
+
+public interface FlightService {
+
+
+	
+	public Flight addFlight(Flight flight);
+	public Flight updateFlight(Flight flight);
+	public boolean deleteFlight(long flightNumber);
+    public List<Flight> ViewAllFlights();
+    public Flight viewFlight(int flightNumber);
+   
+    
+}
+
+   
