d2c4b9e8871f4599c1ab513355a1a2351af825f0
Ojaszw <60997216+Ojaszw@users.noreply.github.com>
2020-05-11 04:49:53 -0700

    Update FlightController.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/controller/FlightController.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/controller/FlightController.java
@@ -2,6 +2,8 @@ package com.capg.fms.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,47 +11,49 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.capg.fms.model.Flight;
 import com.capg.fms.service.FlightService;
+import com.capg.fms.service.FlightServiceImpl;
 
-
-@Component
-@RestController("/FlightManagementSystem")
+@RestController
+@RequestMapping("/FlightManagemetSystem")
 public class FlightController {
 
 	@Autowired
-	FlightService fmsService;
+	public FlightService fmsService;
 	
-	
-	
+
 	/*
 	  * This is a GetMethod(Http) by is used to get  all the flight  from the database.
-	  * Method 	 : viewFlight
+	  * Method 	 : viewAllFlight
 	  * Type 	 : Flight
 	  * parameters: 
 	  * Author 	 : Alok Singh Gaur
 	  * Date 	 : 20/04/2020
-	  * Version   : 1.0
+	  * Version  : 1.0
 	  */
-	@GetMapping("/flight")
-	public List<Flight> viewFlight()
+	@GetMapping("/flightts")
+	public ResponseEntity<List<Flight>> viewAllFlight()
 	{
-		return fmsService.ViewAllFlights();
+		List<Flight> list =fmsService.ViewAllFlights();
+		return new ResponseEntity<List<Flight>>(list,HttpStatus.OK);
 	}
 		
 	
 	 /*
 	  * This is a GetMethod(Http) by flightNumber is used to get  the flight  from the database.
-	  * Method 	 : viewAllFlight
+	  * Method 	 : viewFlight
 	  * Type 	 : Flight
 	  * parameters: flightNumber
 	  * Author 	 : Alok Singh Gaur
 	  * Date 	 : 20/04/2020
 	  * Version   : 1.0
 	  */
-		@GetMapping(value="/flight/{flightNumber}",produces= {"application/json"})
-		public Flight viewAllFlight(@PathVariable int flightNumber)  {
+		@GetMapping(value="/flight/{flightNumber}")
+		public Flight viewFlight(@PathVariable int flightNumber)  {
 			return fmsService.viewFlight(flightNumber);
 		}
 		
@@ -64,13 +68,10 @@ public class FlightController {
 		 * Date 	 : 20/04/2020
 		 * Version   : 1.0
 		 */
-		@PostMapping(value="/flight/add",consumes={"application/json"})
+		@PostMapping( value="/flights")
 		public Flight addFlight(@RequestBody Flight flight) {
-			return fmsService.addFlight(flight);
+			 return fmsService.addFlight(flight);
 		}
-		
-		
-		
 		
 		/*
 		 * This is a DeleteMethod(Http) by "flightNumber" is used to delete the entity from the database.
@@ -97,11 +98,10 @@ public class FlightController {
 		 * Version   : 1.0
 		 */
 		
-		@PutMapping(value="/flight/modify",consumes= {"application/json"})
+		@PutMapping(value="/flight/modify")
 		public Flight modifyFlight(@RequestBody Flight flight) {
 			return fmsService.updateFlight(flight);
 		}
-		
 }
 		
 		
